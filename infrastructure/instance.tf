7b437c0d2c4c3ffc62c6e70b3ff674e4baa3686f
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-11-23 17:11:48 -0500

    test

--- infrastructure/instance.tf
+++ infrastructure/instance.tf
@@ -11,23 +11,23 @@ resource "aws_instance" "machine-1" {
   # the public SSH key
   key_name = aws_key_pair.pk-ec2-1.key_name
 
-  provisioner "file" {
-    source      = "script.sh"
-    destination = "/tmp/script.sh"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/script.sh",
-      "sudo sed -i -e 's/\r$//' /tmp/script.sh",  # Remove the spurious CR characters.
-      "sudo /tmp/script.sh",
-    ]
-  }
-  connection {
-    host        = coalesce(self.public_ip, self.private_ip)
-    type        = "ssh"
-    user        = var.INSTANCE_USERNAME_1
-    private_key = file(var.PATH_TO_PRIVATE_KEY_1)
-  }
+  # provisioner "file" {
+  #   source      = "script.sh"
+  #   destination = "/tmp/script.sh"
+  # }
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "chmod +x /tmp/script.sh",
+  #     "sudo sed -i -e 's/\r$//' /tmp/script.sh",  # Remove the spurious CR characters.
+  #     "sudo /tmp/script.sh",
+  #   ]
+  # }
+  # connection {
+  #   host        = coalesce(self.public_ip, self.private_ip)
+  #   type        = "ssh"
+  #   user        = var.INSTANCE_USERNAME_1
+  #   private_key = file(var.PATH_TO_PRIVATE_KEY_1)
+  # }
 }
 
 resource "aws_instance" "machine-2" {
