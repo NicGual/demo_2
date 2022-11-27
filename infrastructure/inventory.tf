e8fbfc1bc84295831f694628ed10456e1b385ed5
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-11-27 17:37:19 -0500

    generate inventory with terraform

---
+++ infrastructure/inventory.tf
@@ -0,0 +1,7 @@
+resource "local_file" "inventory" {
+    filename = "../ansible/inventory.txt"
+    content     = <<_EOF
+    [deploy]
+    machine1 ansible_host=${aws_instance.machine-1.public_ip}  ansible_ssh_private_key_file=/var/lib/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-1 ansible_ssh_user=ubuntu
+    EOF
+}
