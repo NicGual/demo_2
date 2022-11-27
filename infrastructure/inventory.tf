9e24db3955c2c199997071eeccea96d499fadef6
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-11-27 18:16:02 -0500

    add ansible folder

--- infrastructure/inventory.tf
+++ infrastructure/inventory.tf
@@ -2,6 +2,6 @@ resource "local_file" "inventory" {
     filename = "../ansible/inventory.txt"
     content     = << EOF
     [deploy]
-    machine1 ansible_host=${aws_instance.machine-1.public_ip}  ansible_ssh_private_key_file=/var/lib/jenkins/workspace/"demo pipeline"/infrastructure/keys-machine-1 ansible_ssh_user=ubuntu
+    machine1 ansible_host=${aws_instance.machine-1.public_ip}  ansible_ssh_private_key_file=/var/lib/jenkins/workspace/demo\ pipeline/infrastructure/keys-machine-1 ansible_ssh_user=ubuntu
     EOF
 }
