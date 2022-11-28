ffc90e7879075040ac5ccd0e0c26731f06a5a45a
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-11-28 15:24:32 -0500

    edit provider file

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = var.AWS_REGION
-  shared_config_files      = ["/home/ubuntu/.aws/config"]
-  shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
-  profile = "demo1" 
+  # shared_config_files      = ["/home/ubuntu/.aws/config"]
+  # shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
+  # profile = "demo1" 
 
 }
 
