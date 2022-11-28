780b1efbb79286f68ce5255506e53b898365cae5
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-11-28 15:41:25 -0500

    editing provider file

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,8 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.AWS_REGION
-  # shared_config_files      = ["/home/ubuntu/.aws/config"]
-  # shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
-  # profile = "demo1" 
+  shared_config_files      = ["/home/ubuntu/.aws/config"]
+  shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
+  profile = "demo1" 
 
 }
 
