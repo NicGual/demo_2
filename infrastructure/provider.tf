405fc173da89f7b4ed052de27fa8a773fb9b1b10
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-11-28 23:18:13 -0500

    adding aws keys on jenkins

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -10,7 +10,7 @@ terraform {
 provider "aws" {
   region = var.AWS_REGION
   # shared_config_files      = ["/home/ubuntu/.aws/config"]
-  # shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
+  shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
   # profile = "demo1" 
 
 }
