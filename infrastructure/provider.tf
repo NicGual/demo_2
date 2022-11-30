603e223a60b1d87e62d2c132df92f6c1e4f43fed
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-11-29 19:32:06 -0500

    adding terraform apply

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,17 +1,6 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 4.0"
-#     }
-#   }
-# }
-
 provider "aws" {
   region = var.AWS_REGION
-  # shared_config_files      = ["/home/ubuntu/.aws/config"]
   shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
-  # profile = "demo1" 
 
 }
 
