a11d2973df9d7977cb09c21eec6533158a0dde09
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-11-30 20:04:36 -0500

    tediting inventory and outputs

--- infrastructure/inventory.tf
+++ infrastructure/inventory.tf
@@ -3,6 +3,6 @@ resource "local_file" "inventory" {
     content     = <<-EOF
     [deploy]
     machine1 ansible_host=${aws_instance.machine-1.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-1 ansible_ssh_user=ubuntu
-    machine1 ansible_host=${aws_instance.machine-2.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-1 ansible_ssh_user=ubuntu
+    machine1 ansible_host=${aws_instance.machine-2.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-2 ansible_ssh_user=ubuntu
     EOF
 }
