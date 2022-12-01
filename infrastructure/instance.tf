09217c84451ee5ae512e619cfb1c2369fa6b7093
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-12-01 16:33:04 -0500

    building

--- infrastructure/instance.tf
+++ infrastructure/instance.tf
@@ -37,41 +37,41 @@ resource "aws_instance" "machine-1" {
   
 }
 
-# resource "aws_instance" "machine-2" {
-#   ami           = var.AMIS[var.AWS_REGION]
-#   instance_type = "t2.micro"
+resource "aws_instance" "machine-2" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
 
-#   # the VPC subnet
-#   subnet_id = aws_subnet.main-public-vpc.id
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-vpc.id
 
-#   # the security group
-#   vpc_security_group_ids = [aws_security_group.demo-2-application.id]
+  # the security group
+  vpc_security_group_ids = [aws_security_group.demo-2-application.id]
 
-#   # the public SSH key
-#   key_name = aws_key_pair.pk-ec2-2.key_name
+  # the public SSH key
+  key_name = aws_key_pair.pk-ec2-2.key_name
 
-#   tags = {
-#     Name = "machine-2"
-#   }
+  tags = {
+    Name = "machine-2"
+  }
 
-#   provisioner "file" {
-#     source      = "../config/docker-install.sh"
-#     destination = "/tmp/docker-install.sh"
-#   }
+  provisioner "file" {
+    source      = "../config/docker-install.sh"
+    destination = "/tmp/docker-install.sh"
+  }
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "chmod +x /tmp/docker-install.sh",
-#       "sudo sed -i -e 's/\r$//' /tmp/docker-install.sh",  # Remove the spurious CR characters.
-#       "sudo /tmp/docker-install.sh",
-#     ]
-#   }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/docker-install.sh",
+      "sudo sed -i -e 's/\r$//' /tmp/docker-install.sh",  # Remove the spurious CR characters.
+      "sudo /tmp/docker-install.sh",
+    ]
+  }
 
-#   connection {
-#     host        = coalesce(self.public_ip, self.private_ip)
-#     type        = "ssh"
-#     user        = var.DEFAULT_USERNAME
-#     private_key = file(var.PATH_TO_PRIVATE_KEY_2)
-#   }
+  connection {
+    host        = coalesce(self.public_ip, self.private_ip)
+    type        = "ssh"
+    user        = var.DEFAULT_USERNAME
+    private_key = file(var.PATH_TO_PRIVATE_KEY_2)
+  }
 
-# }
+}
--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -2,6 +2,6 @@ output machine_1_ip {
   value       = aws_instance.machine-1.public_ip
 }
 
-# output machine_2_ip {
-#   value       = aws_instance.machine-2.public_ip
-# }
+output machine_2_ip {
+  value       = aws_instance.machine-2.public_ip
+}
