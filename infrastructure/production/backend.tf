d7903d6bda6440760642578496c1e0071778cf25
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-12-04 18:21:23 -0500

    add alb and change infrastructure folder(edit)

--- infrastructure/production/backend.tf
+++ infrastructure/production/backend.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket" "application-state" {
     server_side_encryption_configuration {
 
         rule {
-            apply_server_side_centryption_by_default {
+            apply_server_side_encryption_by_default {
 
                 sse_algorithm = "AES256"
             }
