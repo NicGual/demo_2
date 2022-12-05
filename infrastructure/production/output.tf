c904d6b4450ef2ea6eb3f7c297257012bf8d6e7a
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-12-05 15:34:40 -0500

    adding 2 ec2 instances and editing playbooks

--- infrastructure/production/output.tf
+++ infrastructure/production/output.tf
@@ -6,6 +6,14 @@ output machine_2_ip {
   value       = aws_instance.machine-2.public_ip
 }
 
+output machine_3_ip {
+  value       = aws_instance.machine-3.public_ip
+}
+
+output machine_4_ip {
+  value       = aws_instance.machine-4.public_ip
+}
+
 output "ecr_url" {
   value = aws_ecr_repository.app-image-storage-production.repository_url
 }
