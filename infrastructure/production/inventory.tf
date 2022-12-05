ce92df75b4f5731989dadfa42138fe4a93c3e0cb
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-12-05 10:30:28 -0500

    building whole infrastructure

--- infrastructure/production/inventory.tf
+++ infrastructure/production/inventory.tf
@@ -1,5 +1,5 @@
 resource "local_file" "inventory" {
-    filename = "../ansible/inventory.txt"
+    filename = "../../ansible/inventory.txt"
     content     = <<-EOF
     [deploy]
     machine1 ansible_host=${aws_instance.machine-1.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-1 ansible_ssh_user=ubuntu
