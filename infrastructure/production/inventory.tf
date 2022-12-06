795b7e62de2be54263c98ef9103c28409d926f75
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-12-06 00:22:19 -0500

    trying install & configure AWS CLI

--- infrastructure/production/inventory.tf
+++ infrastructure/production/inventory.tf
@@ -1,10 +1,10 @@
 resource "local_file" "inventory" {
     filename = "../../ansible/inventory.txt"
     content     = <<-EOF
-    [deploy]
+    [build]
     machine1 ansible_host=${aws_instance.machine-1.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-1 ansible_ssh_user=ubuntu
     machine3 ansible_host=${aws_instance.machine-3.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-3 ansible_ssh_user=ubuntu
-    [build]
+    [deploy]
     machine2 ansible_host=${aws_instance.machine-2.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-2 ansible_ssh_user=ubuntu
     machine4 ansible_host=${aws_instance.machine-4.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-4 ansible_ssh_user=ubuntu
     EOF
