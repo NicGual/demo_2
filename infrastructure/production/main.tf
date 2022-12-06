f3551f1652952625b75227cce59c1ed57a271c0d
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-12-06 17:53:19 -0500

    building infrastructure

--- infrastructure/production/main.tf
+++ infrastructure/production/main.tf
@@ -6,6 +6,6 @@ module alb {
   PUBLIC_SUBNET_1_ID = aws_subnet.main-public-vpc.id
   PUBLIC_SUBNET_2_ID = aws_subnet.main-public-vpc-2.id
   VPC_ID             = aws_vpc.main-application-vpc.id
-  TARGET_INSTANCES   = [aws_instance.machine-1.public_ip,aws_instance.machine-2.public_ip,aws_instance.machine-3.public_ip]
+  TARGET_INSTANCES   = [aws_instance.machine-1.private_ip,aws_instance.machine-2.private_ip,aws_instance.machine-3.private_ip]
   #CERTIFICATE_ARN    = 
 }
