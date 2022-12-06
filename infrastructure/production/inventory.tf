a9ab920e948718f125ac26fe3eb2325581deae6d
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-12-05 23:30:29 -0500

    trying providing files

--- infrastructure/production/inventory.tf
+++ infrastructure/production/inventory.tf
@@ -6,6 +6,6 @@ resource "local_file" "inventory" {
     machine3 ansible_host=${aws_instance.machine-3.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-3 ansible_ssh_user=ubuntu
     [build]
     machine2 ansible_host=${aws_instance.machine-2.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-2 ansible_ssh_user=ubuntu
-    machine1 ansible_host=${aws_instance.machine-4.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-4 ansible_ssh_user=ubuntu
+    machine4 ansible_host=${aws_instance.machine-4.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-4 ansible_ssh_user=ubuntu
     EOF
 }
