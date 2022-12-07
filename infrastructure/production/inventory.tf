a5c003dcf7e3fe43cf0d6595f84574dddc841024
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-12-07 17:02:39 -0500

    buildin production infrastructure

--- infrastructure/production/inventory.tf
+++ infrastructure/production/inventory.tf
@@ -2,9 +2,9 @@ resource "local_file" "inventory" {
     filename = "../../ansible/inventory.txt"
     content     = <<-EOF
     [build]
-    machine1 ansible_host=${aws_instance.machine-1.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-1 ansible_ssh_user=ubuntu
+    machine1 ansible_host=${aws_instance.machine-1.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/production/keys-machine-1 ansible_ssh_user=ubuntu
     [deploy]
-    machine2 ansible_host=${aws_instance.machine-2.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-2 ansible_ssh_user=ubuntu
-    machine3 ansible_host=${aws_instance.machine-3.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/keys-machine-3 ansible_ssh_user=ubuntu
+    machine2 ansible_host=${aws_instance.machine-2.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/production/keys-machine-2 ansible_ssh_user=ubuntu
+    machine3 ansible_host=${aws_instance.machine-3.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/'demo pipeline'/infrastructure/production/keys-machine-3 ansible_ssh_user=ubuntu
     EOF
 }
