0bb420f029038ae01d90c9e368af130ed5206339
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-12-07 13:10:41 -0500

    building infra for dev

--- infrastructure/development/inventory.tf
+++ infrastructure/development/inventory.tf
@@ -2,8 +2,8 @@ resource "local_file" "inventory" {
     filename = "../../ansible/inventory.txt"
     content     = <<-EOF
     [build]
-    machine1 ansible_host=${aws_instance.dev-machine-1.public_ip}  ansible_ssh_private_key_file=dev-keys-machine-1 ansible_ssh_user=ubuntu
+    machine1 ansible_host=${aws_instance.dev-machine-1.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/Demo-2-pipeline_Development/infrastructure/development/dev-keys-machine-1 ansible_ssh_user=ubuntu
     [deploy]
-    machine2 ansible_host=${aws_instance.dev-machine-2.public_ip}  ansible_ssh_private_key_file=dev-keys-machine-2 ansible_ssh_user=ubuntu
+    machine2 ansible_host=${aws_instance.dev-machine-2.public_ip}  ansible_ssh_private_key_file=/home/ubuntu/jenkins/workspace/Demo-2-pipeline_Development/infrastructure/development/dev-keys-machine-2 ansible_ssh_user=ubuntu
     EOF
 }
