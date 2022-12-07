3a60b2a8ad920ec8e92d49afdcc6abd993a934d9
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-12-07 02:03:10 -0500

    building dev infrastructure

--- infrastructure/development/backend.tf
+++ infrastructure/development/backend.tf
@@ -1,8 +1,8 @@
 terraform {
     backend "s3" {
-      bucket    = "dev-branch-state-${var.PROJECT_ID}"
+      bucket    = "dev-branch-state-100283722"
       key       = "dev-infra-state/demo/terraform.tfstate"
-      region    = var.AWS_REGION
+      region    = "us-east-1"
       encrypt   = true
     }
     
