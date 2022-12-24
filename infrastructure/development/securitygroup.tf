408308dae3e06c7b90981ca127fa3d1042dbdb6f
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-12-24 16:32:20 -0500

    DD2-9 deploying Node Exporter

--- infrastructure/development/securitygroup.tf
+++ infrastructure/development/securitygroup.tf
@@ -17,6 +17,13 @@ resource "aws_security_group" "dev-env-sg" {
   }
 
   ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
